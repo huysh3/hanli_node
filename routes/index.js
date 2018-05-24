87c534bc4dbff6a10fccfc1d7cf454817498751d
Huysh3 <734645956@qq.com>
2018-05-25 03:18:56 +0800

    dev

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
-  res.render('login.ejs');
+  res.render('index.ejs');
 });
 
 module.exports = router;
