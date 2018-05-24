57edda4e923f3133e1edf00f5201870e7d5a88ac
Huysh3 <734645956@qq.com>
2018-05-25 04:07:57 +0800

    dev

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/login', function(req, res, next) {
   res.render('index.ejs');
 });
 
+router.get('/layout', function(req, res, next) {
+  res.render('layout.jade');
+});
+
 module.exports = router;
